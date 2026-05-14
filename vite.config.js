fbd4dcec1e52c826676ee495ee5b3679ef96a429
Braulio Bhavamitra <brauliobo@gmail.com>
2026-05-14 10:10:33 -0300

    pages: publish build to /docs for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -27,7 +27,8 @@ export default defineConfig({
   },
   server: { port: 5173, open: false },
   build: {
-    outDir: 'dist',
+    outDir: 'docs',
+    emptyOutDir: true,
     sourcemap: false,
   },
 })
